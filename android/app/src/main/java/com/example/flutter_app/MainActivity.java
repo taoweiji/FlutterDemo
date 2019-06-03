0884bb77038484e96e0b46566b4ca2ccfefda17e
Wiki <taoweiji2008@qq.com>
2019-06-03 13:32:31 +0800

    first commit.

---
+++ android/app/src/main/java/com/example/flutter_app/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.flutter_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.EventChannel;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+    }
+}
---
+++ publish_android_aar.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# publish_android_aar.sh
+
+# 我们用于打包aar的module名称
+myFlutterModule="myflutter"
+
+
+echo "Clean old build"
+find . -d -name "build" | xargs rm -rf
+flutter clean
+
+echo "Get packages"
+flutter packages get
+
+echo "Build release AOT"
+flutter build aot --release  --output-dir=build/flutteroutput/aot
+echo "Build release Bundle"
+flutter build bundle --precompiled  --asset-dir=build/flutteroutput/flutter_assets
+
+# 复制插件生成的GeneratedPluginRegistrant.java到我们需要打包的module
+echo 'Copy GeneratedPluginRegistrant.java to module'
+mkdir -p android/${myFlutterModule}/src/main/java/io/flutter/plugins && cp android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java "$_"
+
+# 将依赖的插件打包发布到本地或者远程的maven仓库，需要修改android/build.gradle
+echo 'Build and publish module to repo'
+cd android
+gradlewScript=""
+file="../.flutter-plugins"
+while read line
+do
+    array=(${line//=/ })
+    moduleName=${array[0]}
+    gradlewScript="$gradlewScript:${moduleName}:clean :${moduleName}:uploadArchives "
+done < ${file}
+gradlewScript=${gradlewScript}":${myFlutterModule}:clean :${myFlutterModule}:uploadArchives "
+echo "./gradlew ${gradlewScript}"
+./gradlew ${gradlewScript}
